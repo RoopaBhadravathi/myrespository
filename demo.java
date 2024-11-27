da35cd62fc4ec2f6bf667cf932a8e27ff207803d
RoopaBhadravathi <hkroopa27b@gmail.com>
2024-11-27 14:06:48 +0530

    Update demo1.java

--- demo.java
+++ demo.java
@@ -4,5 +4,11 @@ class demo
      public static void main(String args[])
      {
 	System.out.println("hello");
+	     display();
+	     public static void display()
+	     {
+		     	System.out.println("welcome");
+	     }
+		     
      }
 }
